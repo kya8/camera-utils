f64f228984220b9256fda467d792fae0c7d524b2
kya <46643054+kya8@users.noreply.github.com>
2026-01-12 01:29:01 +0800

    Fix typo

--- libs/common/version_print.cpp
+++ libs/common/version_print.cpp
@@ -3,7 +3,7 @@
 
 void camera_utils::version::print_info() noexcept
 {
-    std::printf("Version %s, %s. Built type %s, %s %s, compiled with %s %s\n",
+    std::printf("Version %s, %s. Build type %s, %s %s, compiled with %s %s\n",
                 GIT_DESC, COMMIT_DATE,
                 BUILD_TYPE,
                 TARGET_OS, TARGET_ARCH,
