1c4626e8b10f9d45319fd5790205fbd8d321642d
kya <46643054+kya8@users.noreply.github.com>
2026-03-30 23:43:49 +0800

    mp4_merge: Make callback argument optional; Update docs

--- libs/mp4utils/mp4_merge.hpp
+++ libs/mp4utils/mp4_merge.hpp
@@ -5,13 +5,16 @@ namespace mp4utils {
 
 enum class MergeResult {
     Success = 0,
-    InvalidInput,
-    IoError,
-    InternalError
+    InvalidInput, // Input files do not look like valid MP4 files.
+                  // This is returned by an initial shallow check. 
+    IoError,      // An I/O error occurred.
+    InternalError // An internal error occurred.
+                  // This is mostly likely caused by bad/invalid input files,
+                  // e.g. input files are valid MP4 files, but they are not compatible for merging.
 };
 
 struct MergeProgCb {
-    void(*cb)(void*, int);
+    void(*cb)(void*, int) = nullptr;
     void* data;
 
     operator bool() const noexcept {
@@ -23,8 +26,19 @@ struct MergeProgCb {
     }
 };
 
-// Merge split/chaptered MP4/ISOBMFF files. Input files are assumed to have identical structures and settings.
-MergeResult merge_mp4(int nb_input, const char* const* input_files, const char* output_file, MergeProgCb prog_cb) noexcept;
+/**
+ * Merge split/chaptered MP4 video files.
+ * The input files are assumed to be in the correct order and have identical configurations (e.g., resolution, codec, etc.).
+ * 
+ * @param[in] nb_input Number of input files
+ * @param[in] input_files Array of input file paths. Each path is a null-terminated string, in platform-native narrow encoding.
+ * @param[in] output_file Output file path. Follows the same encoding rules as input files.
+ * @param[in] prog_cb Optional callback for progress updates. If not empty, the callback will be called with
+ *                    the current progress percentage.
+ *
+ * @return MergeResult indicating the result of the operation.
+ */
+MergeResult merge_mp4(int nb_input, const char* const* input_files, const char* output_file, MergeProgCb prog_cb = {}) noexcept;
 
 }
 
