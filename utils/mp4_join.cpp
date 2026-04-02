6ad87ade80ddc7a1366af1c6fd15e55a0faf5410
kya <46643054+kya8@users.noreply.github.com>
2026-04-02 15:56:07 +0800

    mp4_join: Improve command line interface

--- utils/mp4_join.cpp
+++ utils/mp4_join.cpp
@@ -7,12 +7,41 @@
 #include <vector>
 #include <thread>
 #include <atomic>
+#include <filesystem>
+#include "sys_utils.hpp"
 
+namespace fs = std::filesystem;
+
+namespace {
+
+bool get_yn(bool default_val = false) noexcept
+{
+    const auto ans = std::getchar();
+    if (default_val == false) {
+        return ans == 'y' || ans == 'Y';
+    }
+    return ans != 'n' && ans != 'N';
+}
+
+const auto help_msg =
+R"(^^(mp4join: Utility for joining consecutive MP4 files.
+
+Usage: mp4join <FILE_1> <FILE_2> [...] [-o OUTPUT]
+
+Options:
+  -o, --output <OUTPUT>  Specify output file path.
+  -f, --force            Overwrite existing output file without asking for confirmation.
+  -V, --version          Print version information and exit.
+  -h, --help             Print this help message and exit.
+^^)";
+
+} // namespace
 
 int main_mp4_join(int argc, char** argv) noexcept
 {
     std::vector<const char*> inputs;
-    const char* output = nullptr;
+    fs::path output;
+    bool force_overwrite = false;
 
     {
         bool print_version = false;
@@ -21,12 +50,15 @@ int main_mp4_join(int argc, char** argv) noexcept
             if (match(argv[i], "-o", "--output")) {
                 if (++i < argc) output = argv[i];
                 else err_flag = 1;
-            }
-            else if (match(argv[i], "-V", "--version")) {
+            } else if (match(argv[i], "-f", "--force")) {
+                force_overwrite = true;
+            } else if (match(argv[i], "-V", "--version")) {
                 print_version = true;
                 break;
-            }
-            else {
+            } else if (match(argv[i], "-h", "--help")) {
+                std::println("{}", help_msg);
+                return 0;
+            } else {
                 inputs.emplace_back(argv[i]);
             }
             if (err_flag) break;
@@ -37,9 +69,30 @@ int main_mp4_join(int argc, char** argv) noexcept
             print_info();
             return 0;
         }
-        if (err_flag || !output || inputs.size() < 2) {
-            std::println("Usage: mp4_join <file_1> <file_2> [...] <-o output_file> [-V]");
+        if (err_flag || inputs.size() < 2) {
+            std::print("Invalid arguments.\nPass '-h' for help.\n");
             return 2;
+        }
+    }
+    
+    if (output.empty()) {
+        output = inputs.front();
+        output.replace_filename(output.stem().concat("_joined") += (output.extension()));
+    }
+    const auto output_str = output.string();
+
+    const bool stdout_is_tty = sys::is_tty(stdout);
+
+    if (!force_overwrite && fs::exists(output)) {
+        if (stdout_is_tty) { // prompt the user
+            std::print("Output file {} already exists. Overwrite? [y/N] ", output_str);
+            if (!get_yn(false)) {
+                std::println("Aborting.");
+                return 0;
+            }
+        } else { // can't ask, refuse to proceed
+            std::println("Refusing to overwrite existing output file {}.", output_str);
+            return 1;
         }
     }
 
@@ -54,31 +107,34 @@ int main_mp4_join(int argc, char** argv) noexcept
     };
     std::thread worker {
         [&] {
-            ret = merge_mp4(static_cast<int>(inputs.size()), inputs.data(), output, {prog_fn, &prog});
+            ret = merge_mp4(static_cast<int>(inputs.size()), inputs.data(), output_str.c_str(), {prog_fn, &prog});
             done.store(true, std::memory_order_release);
         }
     };
 
-    static constexpr int busy_spin_cnt = 5;
-    static constexpr int busy_spin_interval = 1;
-    static constexpr int relaxed_spin_interval = 100;
-    for (int cnt = 0; !done.load(std::memory_order_acquire); cnt += (cnt < busy_spin_cnt), std::this_thread::sleep_for(std::chrono::milliseconds(cnt < busy_spin_cnt ? busy_spin_interval : relaxed_spin_interval))) {
-        const auto prog_new = prog.load(std::memory_order_acquire);
-        if (prog_new > prog_prev) {
-            std::print("\rProgress: {}%", prog_new);
-            std::fflush(stdout);
-            prog_prev = prog_new;
+    if (stdout_is_tty) {
+        std::fputs("\x1b[?25l", stdout); // hide cursor
+        static constexpr int busy_spin_cnt = 5;
+        static constexpr int busy_spin_interval = 1;
+        static constexpr int relaxed_spin_interval = 100;
+        for (int cnt = 0; !done.load(std::memory_order_acquire); cnt += (cnt < busy_spin_cnt), std::this_thread::sleep_for(std::chrono::milliseconds(cnt < busy_spin_cnt ? busy_spin_interval : relaxed_spin_interval))) {
+            const auto prog_new = prog.load(std::memory_order_acquire);
+            if (prog_new > prog_prev) {
+                std::print("\rProgress: {}%", prog_new);
+                std::fflush(stdout);
+                prog_prev = prog_new;
+            }
         }
+        // Clear the progress line and show cursor again.
+        std::fputs("\x1b[2K\r\x1b[?25h", stdout);
     }
 
     worker.join();
 
-    std::putchar('\r');
-
     using enum MergeResult;
     switch (ret) {
     case(Success):
-        std::println("Merge done: {}", output);
+        std::println("Merge done: {}", output_str);
         break;
     case(InvalidInput):
         std::println("Merge error: Invalid input file.");
