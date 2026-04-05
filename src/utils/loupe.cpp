5df6c094f7b02b34a46c6edcc41e5638739eadad
kya <46643054+kya8@users.noreply.github.com>
2026-04-06 04:50:20 +0800

    loupe cli: Add option to display user-specified property

--- src/utils/loupe.cpp
+++ src/utils/loupe.cpp
@@ -10,9 +10,9 @@
 #include <cstdlib>
 #include "version.hpp"
 #include "sys_utils.hpp"
-#include <range.hpp>
 #include "string_utils.hpp"
 #include <iostream>
+#include <ranges>
 
 using namespace slate::loupe;
 
@@ -24,16 +24,74 @@ R"^^(🔍 loupe: Extract and display camera information from video files.
 Usage: loupe <FILES...> [OPTIONS...]
 
 Options:
- -m, --metadata-only  Only extract metadata, skip sensor data and other streaming data.
- -j, --raw-output     Output a raw JSON-like format for easier parsing.
- -g, --dump-gyro      Dump gyroscope data (if available) in CSV format for each input.
- -q, --dump-quat      Dump camera quaternion data (if available) in CSV format.
- -l <N>               Limit the number of elements printed for vector types to N. Default is 50. Set to 0 for no limit.
-                      Does not affect '-j' output, which always prints the full data.
- --                   Treat all following arguments as input file names, even if they start with '-'.
- -V, --version        Display version information and exit.
- -h, --help           Display this help message and exit.
+ -m, --metadata-only    Only extract metadata, skip sensor data and other streaming data.
+ -j, --raw-output       Output a raw JSON-like format for easier parsing.
+ -g, --dump-gyro        Dump gyroscope data (if available) in CSV format for each input.
+ -q, --dump-quat        Dump camera quaternion data (if available) in CSV format.
+ -l <N>                 Limit the number of elements printed for vector types to N. Default is 50. Set to 0 for no limit.
+                        Does not affect '-j' output, which always prints the full data.
+ -p, --property <PROP>  For each input video, only display the specified property.
+                        PROP must consist of a group name and at least one key name, separated by colons (:),
+                        e.g., Group:Key1:Key2
+ --                     Treat all following arguments as input file names, even if they start with '-'.
+ -V, --version          Display version information and exit.
+ -h, --help             Display this help message and exit.
 )^^";
+
+GroupId find_group(std::string_view str) noexcept
+{
+    for (int i = 0; i < std::to_underlying(GroupId::Max_GroupId); ++i) {
+        const auto group_id = static_cast<GroupId>(i);
+        if (to_string(group_id) == str)
+            return group_id;
+    }
+    return GroupId::Max_GroupId;
+}
+
+KeyId find_key(std::string_view str) noexcept
+{
+    for (int i = 0; i < std::to_underlying(KeyId::Max_KeyId); ++i) {
+        const auto key_id = static_cast<KeyId>(i);
+        if (to_string(key_id) == str)
+            return key_id;
+    }
+    return KeyId::Max_KeyId;
+}
+
+// Find property value
+// prop = "Group:Key1:Key2"
+const Value* find_value(const GroupedVarMap& gmap, std::string_view prop) noexcept
+{
+    bool is_group = true;
+    const VarMap* map = nullptr;
+    const Value* val = nullptr;
+    for (const auto key : std::views::split(prop, std::string_view(":"))) {
+        const std::string_view sv(key);
+        if (is_group) {
+            if (const auto group = find_group(sv); group == GroupId::Max_GroupId) {
+                return nullptr;
+            } else {
+                map = gmap.get(group);
+            }
+            is_group = false;
+        } else {
+            // Keys. Must have previous map.
+            if (!map) {
+                return nullptr;
+            }
+            if (const auto key = find_key(sv); key == KeyId::Max_KeyId) {
+                val = map->get_value(sv);
+            } else {
+                val = map->get_value(key);
+            }
+            if (!val)
+                return nullptr;
+            map = std::get_if<VarMap>(val);
+        }
+    }
+
+    return val;
+}
 
 } // namespace
 
@@ -47,6 +105,7 @@ int slate::main_loupe(int argc, char** argv) noexcept
     bool print_version = false;
     bool show_help = false;
     bool raw_output = false;
+    const char* show_property = nullptr;
     {
         bool err_flag = 0;
         bool positional_only = false;
@@ -63,6 +122,12 @@ int slate::main_loupe(int argc, char** argv) noexcept
                 dump_gyro = true;
             } else if (match(argv[i], "-q", "--dump-quat")) {
                 dump_quat = true;
+            } else if (match(argv[i], "-p", "--property")) {
+                if (++i == argc) {
+                    err_flag = 1;
+                } else {
+                    show_property = argv[i];
+                }
             } else if (match(argv[i], "-l")) {
                 if (++i < argc) {
                     char* end;
@@ -103,13 +168,15 @@ int slate::main_loupe(int argc, char** argv) noexcept
 
     util::Timer timer;
     const bool stdout_is_colorterm = sys::is_colorterm(stdout);
-    for (const auto i : range::make_index(files.size())) {
-        const auto& file = files[i];
+    bool first = true;
+    for (const auto& file : files) {
         timer.tic();
         const auto info = detect(file, metadata_only);
         const auto time_detect = timer.toc();
 
-        if (i > 0) std::putchar('\n');
+        if (!first)
+            std::putchar('\n');
+        first = false;
         if (stdout_is_colorterm)
             std::print("\033[1;32m{}\033[0m", file);
         else
@@ -120,24 +187,34 @@ int slate::main_loupe(int argc, char** argv) noexcept
             std::print(": Failed to extract data, or input file is invalid.\n");
 
         if (info) {
-            static constexpr auto width = 30;
-            std::print("{:<{}}: {}\n", "Camera Vendor", width, to_string(info->vendor));
-
-            for (const auto& [group, map] : info->extras) {
-                if (stdout_is_colorterm)
-                    std::print("\033[33m");
-                std::print("<Group: {}>\n", to_string(group));
-                if (stdout_is_colorterm)
-                    std::print("\033[0m" );
-                if (raw_output) {
-                    std::cout << map << '\n';
+            if (show_property) {
+                const auto val = find_value(info->extras, show_property);
+                // Display the property
+                if (!val) {
+                    std::println("{}: Property not found!", show_property);
                 } else {
-                    for (const auto& [key, value] : map) {
-                        if (stdout_is_colorterm)
-                            std::print("  \033[36m{:<{}}\033[0m", to_string(key), width - 2);
-                        else
-                            std::print("  {:<{}}", to_string(key), width - 2);
-                        std::print(": {}\n", to_string(value, max_vec_len));
+                    std::println("{}: {}", show_property, to_string(*val));
+                }
+            } else {
+                static constexpr auto width = 30;
+                std::print("{:<{}}: {}\n", "Camera Vendor", width, to_string(info->vendor));
+
+                for (const auto& [group, map] : info->extras) {
+                    if (stdout_is_colorterm)
+                        std::print("\033[33m");
+                    std::print("<Group: {}>\n", to_string(group));
+                    if (stdout_is_colorterm)
+                        std::print("\033[0m" );
+                    if (raw_output) {
+                        std::cout << map << '\n';
+                    } else {
+                        for (const auto& [key, value] : map) {
+                            if (stdout_is_colorterm)
+                                std::print("  \033[36m{:<{}}\033[0m", to_string(key), width - 2);
+                            else
+                                std::print("  {:<{}}", to_string(key), width - 2);
+                            std::print(": {}\n", to_string(value, max_vec_len));
+                        }
                     }
                 }
             }
