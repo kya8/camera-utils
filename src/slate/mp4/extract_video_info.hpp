a65bdfb390a3e21fe0864e8c8f1b0c1aa76123c3
kya <46643054+kya8@users.noreply.github.com>
2026-04-06 16:11:54 +0800

    Document extract_video_info

--- src/slate/mp4/extract_video_info.hpp
+++ src/slate/mp4/extract_video_info.hpp
@@ -7,6 +7,14 @@
 
 namespace slate::mp4 {
 
+/**
+ * Extract video info from an MP4 stream.
+ * @param[in] file  An MP4 stream opened for reading.
+ * @param[out] out  Video info to write to.
+ * @param[out] video_track_ids  Optional reference to an output vector for the indices of video tracks in the MP4.
+ *
+ * @return Whether the extraction was successful.
+ */
 bool extract_video_info(Mp4Stream& file, VideoInfo& out, std::vector<int>* video_track_ids = nullptr) noexcept;
 
 } /* namespace slate::mp4 */
